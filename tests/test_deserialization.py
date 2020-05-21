87ec37c56a128d4ada3d0a0b6672c438445405ee
erdem <github@erdemozkol.com>
2020-05-21 01:40:46 +0100

    Tested related fields deserialization with django related fields

--- tests/test_deserialization.py
+++ tests/test_deserialization.py
@@ -30,20 +30,12 @@ def o2o_related_instance(db_models):
     return one_to_one_instance
 
 
-@pytest.mark.parametrize(
-    'schema_fields_option',
-    [
-        '__all__',
-        ('name', 'foreign_key_field', 'many_to_many_field', 'one_to_one_field')
-    ]
-)
-def test_deserialization_related_primary_key_fields(
+def test_related_fields_deserialization(
     db,
     db_models,
     fk_related_instance,
     o2o_related_instance,
-    m2m_related_instance,
-    schema_fields_option
+    m2m_related_instance
 ):
     """
         Load a schema has related fields, the schema related fields should get primary keys as value.
@@ -51,25 +43,53 @@ def test_deserialization_related_primary_key_fields(
     class TestSchema(ModelSchema):
         class Meta:
             model = db_models.AllRelatedFieldsModel
-            fields = schema_fields_option
+            fields = ('name', 'foreign_key_field', 'many_to_many_field', 'one_to_one_field')
 
     schema = TestSchema()
 
-    assert db_models.AllRelatedFieldsModel.objects.count() == 0
+    load_data = {
+        'name': 'Deserialized Instance',
+        'foreign_key_field': {'id': fk_related_instance.id},
+        'one_to_one_field': {'uuid': str(o2o_related_instance.uuid)},
+        'many_to_many_field': [{'uuid': str(m2m_related_instance.uuid)}]
+    }
+
+    deserialized_data = schema.load(load_data)
+
+    assert deserialized_data['name'] == 'Deserialized Instance'
+    assert deserialized_data['foreign_key_field'].id == fk_related_instance.id
+    assert deserialized_data['many_to_many_field'][0].uuid == m2m_related_instance.uuid
+    assert deserialized_data['one_to_one_field'].uuid == o2o_related_instance.uuid
+
+
+def test_related_pk_fields_deserialization(
+    db,
+    db_models,
+    fk_related_instance,
+    o2o_related_instance,
+    m2m_related_instance,
+):
+    """
+        Load a schema has related fields, the schema related fields should get primary keys as value.
+    """
+    class TestSchema(ModelSchema):
+        class Meta:
+            model = db_models.AllRelatedFieldsModel
+            fields = ('name', 'foreign_key_field', 'many_to_many_field', 'one_to_one_field')
+            use_related_pk_fields = True
+
+    schema = TestSchema()
 
     load_data = {
         'name': 'Deserialized Instance',
         'foreign_key_field': fk_related_instance.id,
-        'one_to_one_field': o2o_related_instance.uuid,
-        'many_to_many_field': [m2m_related_instance.uuid]
+        'one_to_one_field': str(o2o_related_instance.uuid),
+        'many_to_many_field': [str(m2m_related_instance.uuid)]
     }
 
-    validated_data = schema.load(load_data)
-    instance = schema.save(validated_data)
+    deserialized_data = schema.load(load_data)
 
-    entity = db_models.AllRelatedFieldsModel.objects.first()
-    assert db_models.AllRelatedFieldsModel.objects.count() == 1
-    assert instance.name == entity.name
-    assert instance.foreign_key_field.id == entity.foreign_key_field.id
-    assert instance.many_to_many_field.first().uuid == entity.many_to_many_field.first().uuid
-    assert instance.one_to_one_field.uuid == entity.one_to_one_field.uuid
+    assert deserialized_data['name'] == 'Deserialized Instance'
+    assert deserialized_data['foreign_key_field'].id == fk_related_instance.id
+    assert deserialized_data['many_to_many_field'][0].uuid == m2m_related_instance.uuid
+    assert deserialized_data['one_to_one_field'].uuid == o2o_related_instance.uuid
