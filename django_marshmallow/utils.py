f0b3067ce5dc19016c661e5985dcb6a3741ab8a8
erdem <github@erdemozkol.com>
2020-06-02 23:22:51 +0100

    remove construct_instance utility method

--- django_marshmallow/utils.py
+++ django_marshmallow/utils.py
@@ -168,42 +168,6 @@ def is_abstract_model(model):
     return hasattr(model, '_meta') and hasattr(model._meta, 'abstract') and model._meta.abstract
 
 
-def construct_instance(form, instance, fields=None, exclude=None):
-    """
-    Construct and return a model instance from the bound ``form``'s
-    ``cleaned_data``, but do not save the returned instance to the database.
-    """
-    from django.db import models
-    opts = instance._meta
-
-    cleaned_data = form.cleaned_data
-    file_field_list = []
-    for f in opts.fields:
-        if not f.editable or isinstance(f, models.AutoField) \
-                or f.name not in cleaned_data:
-            continue
-        if fields is not None and f.name not in fields:
-            continue
-        if exclude and f.name in exclude:
-            continue
-        # Leave defaults for fields that aren't in POST data, except for
-        # checkbox inputs because they don't appear in POST data if not checked.
-        if (f.has_default() and
-                form[f.name].field.widget.value_omitted_from_data(form.data, form.files, form.add_prefix(f.name))):
-            continue
-        # Defer saving file-type fields until after the other fields, so a
-        # callable upload_to can use the values from other fields.
-        if isinstance(f, models.FileField):
-            file_field_list.append(f)
-        else:
-            f.save_form_data(instance, cleaned_data[f.name])
-
-    for f in file_field_list:
-        f.save_form_data(instance, cleaned_data[f.name])
-
-    return instance
-
-
 def modelschema_factory(model, schema=ModelSchema, fields=None, exclude=None, **kwargs):
     """
         Return a ModelSchema containing schema fields for the given model.
